3f4c068870ebd8d470ca511bcaa3f52cdc5148d2
emilki <66437606+emilki@users.noreply.github.com>
2024-05-25 19:57:06 -0400

    Create potato.py

---
+++ potato.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from aiogram.utils import executor
+
+API_TOKEN = '6626086371:AAHYxRs7aOFn1p_NawCyVPcMV2Yg1Q3RKUI'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton("Start Game", web_app=WebAppInfo(url="https://yourgameurl.com")))
+    await message.reply("Welcome to Potato Clicker! Click the button below to start the game.", reply_markup=keyboard)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
